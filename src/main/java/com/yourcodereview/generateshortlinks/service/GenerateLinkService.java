d86beb980b6cf8a9f1f26c59f2fb3719cb4afc98
Dmitriy Erokhin <nefariusmag@gmail.com>
2022-06-22 21:05:17 +0300

    Change method to Optional

--- src/main/java/com/yourcodereview/generateshortlinks/service/GenerateLinkService.java
+++ src/main/java/com/yourcodereview/generateshortlinks/service/GenerateLinkService.java
@@ -4,26 +4,24 @@ import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class GenerateLinkService {
 
-    private final LinkService linkService;
     private static final int LENGTH_ENCRYPTING = 7;
+    private final LinkService linkService;
 
     public String generateShortLink(String originalLink) {
 
-        String shortLink = linkService.getShortLink(originalLink);
-        if (shortLink != null) {
-            return shortLink;
-        }
-
-        shortLink = generateNewUniqShortLink();
-        linkService.saveLink(originalLink, shortLink);
-        return shortLink;
+        return Optional.ofNullable(linkService.getShortLink(originalLink))
+                .orElseGet(() -> generateNewUniqShortLink(originalLink));
     }
 
-    private String generateNewUniqShortLink() {
-        return RandomStringUtils.randomAlphabetic(LENGTH_ENCRYPTING);
+    private String generateNewUniqShortLink(String originalLink) {
+        String shortLink = RandomStringUtils.randomAlphabetic(LENGTH_ENCRYPTING);
+        linkService.saveLink(originalLink, shortLink);
+        return shortLink;
     }
 }
